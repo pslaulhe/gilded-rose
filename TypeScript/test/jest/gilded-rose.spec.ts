77822028621830640cd7d6d81e3f9093e5d4ce9f
aurelien <aurelien.dauchot@soprism.com>
2024-04-18 12:12:37 +0200

    Full test coverage of gilded rose file

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,6 +1,45 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
+  // Common items
+  it('should decrease quality of item when sell date is not passed', () => {
+    const gildedRose = new GildedRose([new Item('Whatever', 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(9);
+  });
+
+  it('should decrease quality of item when sell date is passed', () => {
+    const gildedRose = new GildedRose([new Item('Whatever', 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(8);
+  });
+
+  it('should decrease sellin by 1 for normal items', () => {
+    const gildedRose = new GildedRose([new Item('Whatever', 5, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(4);
+  });
+
+  // Backstage item
+  it('should increase quality of backstage passes when sell date lower or equals to 10 and greater than 5', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(2);
+  });
+
+  it('should increase quality of backstage passes when sell date inferior or equals to 5 and greater than 0', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+  });
+
+  it('should increase quality of backstage passes when sell date is 0', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  // Age Brie item
   it('should increase quality of aged brie when sell date is passed', () => {
     const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -11,5 +50,17 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([new Item('Aged Brie', 5, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(1);
+  });
+
+  it('should not decrease sellin when item is Sulfuras', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(5);
+  });
+
+  it('should not update the quality of Sulfuras', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
   });
 });
