5addfed40afd14f8b1d202d73d4a6174980753f0
aurelien <aurelien.dauchot@soprism.com>
2024-04-18 11:53:20 +0200

    Test coverage of aged brie when date is passed and not passed

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,15 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should increase quality of aged brie when sell date is passed', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].quality).toBe(2);
+  });
+
+  it('should increase quality of aged brie when sell date is not passed', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 5, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
   });
 });
